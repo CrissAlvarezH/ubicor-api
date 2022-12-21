447d31806e187a01934aef770e16294ae151732e
Cristian Alvarez H <alvarezhernandezcristian1@gmail.com>
2022-12-21 00:19:47 -0500

    :wrench: add command to upload images for unicor university

--- app/universities/commands/__init__.py
+++ app/universities/commands/__init__.py
@@ -1,6 +1,7 @@
 import click
 
 from .insert_initial_data import insert_initial_data
+from .upload_unicor_imgs import upload_unicor_imgs
 
 
 @click.group()
@@ -9,3 +10,4 @@ def cli():
 
 
 cli.add_command(insert_initial_data)
+cli.add_command(upload_unicor_imgs)
---
+++ app/universities/commands/upload_unicor_imgs.py
@@ -0,0 +1,46 @@
+import json
+import os
+
+import click
+import requests
+
+
+@click.command()
+@click.argument("domain")
+@click.argument("user")
+@click.argument("password")
+def upload_unicor_imgs(domain, user, password):
+    BASE_URL = f"http://{domain}/api/v1" 
+
+    # get token
+    response = requests.post(f"{BASE_URL}/login", data={"username": user, "password": password})
+    if response.status_code != 200:
+        print("ERROR: login error", response.text)
+        return
+    access_token = response.json().get("access_token")
+
+    response = requests.get(f"{BASE_URL}/universities/unicor/buildings")
+
+    for building in response.json():
+        print("\n")
+        # get images from folder called equals to building code
+        path = os.path.join(os.path.dirname(__file__), "ubicor_imgs", building.get("code"))
+        if not os.path.exists(path):
+            print("does not exists", building.get("code"), building.get("name"))
+            continue
+
+        print("------ upload imgs from building:", building.get("code"), " -------")
+
+        for img_name in os.listdir(path):
+            img_path = os.path.join(path, img_name)
+
+            with open(img_path, 'rb') as f:
+                response = requests.post(
+                    url=f"{BASE_URL}/universities/unicor/buildings/{building.get('id')}/images/",
+                    headers={"Authorization": f"Bearer {access_token}"},
+                    files={"files": f}    
+                )
+                print("upload", img_name, "response code", response.status_code)
+                if response.status_code != 201:
+                    print("response body:", response.text)
+
